54300085c5507694d516c14a60ca6e374bfeb943
swapnil <swapnil.kurve@saama.com>
2020-04-23 13:18:11 +0530

    Added new APIs for delete post and posts on the basis of userProfile

--- demo/src/main/java/com/example/demo/repo/IPostRepo.java
+++ demo/src/main/java/com/example/demo/repo/IPostRepo.java
@@ -1,6 +1,9 @@
 package com.example.demo.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Posts;
@@ -8,4 +11,6 @@ import com.example.demo.model.Posts;
 @Repository
 public interface IPostRepo extends JpaRepository<Posts, Long> {
 
+	@Query("FROM Posts p where p.userProfile.id = :userProfile")
+	List<Posts> findByUserProfile(Long userProfile);
 }
--- demo/src/main/java/com/example/demo/rest/PostController.java
+++ demo/src/main/java/com/example/demo/rest/PostController.java
@@ -1,16 +1,19 @@
 package com.example.demo.rest;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Posts;
@@ -31,7 +34,7 @@ public class PostController {
 
 	@Autowired
 	IPostRepo postRepo;
-	
+
 	@Autowired
 	ITagsRepo tagRepo;
 
@@ -48,7 +51,7 @@ public class PostController {
 			p.setUserProfile(up);
 
 			Set<Tags> tags = new HashSet<>();
-			
+
 			for (TagsDetails s : post.getTags()) {
 				Tags tag = tagRepo.getTagByName(s.getName());
 				if (tag == null) {
@@ -75,9 +78,29 @@ public class PostController {
 			return String.valueOf(p.getId());
 	}
 
-	@GetMapping("/getAllPosts")
-	public List<Posts> getAllPosts() {
-		return postRepo.findAll();
+	@GetMapping("/{userProfileId}/getAllPosts")
+	public List<PostDetails> getAllPosts(@PathVariable(value = "userProfileId") String id) {
+		List<PostDetails> posts = new ArrayList<>();
+		Set<TagsDetails> tags = new HashSet<>();
+		for (Posts p : postRepo.findByUserProfile(Long.parseLong(id))) {
+			tags.clear();
+			for (Tags t : p.getTags()) {
+				tags.add(new TagsDetails(t.getId(), t.getName()));
+			}
+			posts.add(new PostDetails(p.getTitle(), p.getDescription(), tags));
+		}
+		return posts;
+	}
+
+	@DeleteMapping("/{userProfileId}/removePostById")
+	public void removePostById(@PathVariable(value = "userProfileId") String userProfileId,
+			@RequestParam(value = "postId") String id) {
+
+		Optional<Posts> posts = postRepo.findById(Long.parseLong(id));
+		if (posts.isPresent() && (posts.get().getUserProfile().getId() == Long.parseLong(userProfileId))) {
+			postRepo.deleteById(Long.parseLong(id));
+		}
+
 	}
 
 }
