0676a3da9d7440eeada704151aba195a4dfc982e
aartighugarkar <aartighugarkar@gmail.com>
2020-04-26 14:04:28 +0530

    api end point change

--- demo/src/main/resources/static/service/SignUpService.js
+++ demo/src/main/resources/static/service/SignUpService.js
@@ -3,7 +3,7 @@ appName.service("SignUpService", function($http,$q){
 	this.saveUser = function(user){		
 
 		var defer = $q.defer();
-		return $http.post('http://admin:admin123@localhost:8080/users/addUser',JSON.stringify(user))		
+		return $http.post('http://admin:admin123@localhost:8080/users',JSON.stringify(user))		
 			.then(function(response) {
 				defer.resolve(response.data);
 				return defer.promise;
