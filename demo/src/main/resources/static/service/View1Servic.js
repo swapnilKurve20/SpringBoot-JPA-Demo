2de81955854dcdb80965e844b147e756b9da8a36
aartighugarkar <aartighugarkar@gmail.com>
2020-04-28 12:11:44 +0530

    posts likes and tags, Global URL base, All posts button in profile page

--- demo/src/main/resources/static/service/View1Servic.js
+++ demo/src/main/resources/static/service/View1Servic.js
@@ -1,9 +1,11 @@
-appName.service("View1Service", function($http,$q){
+appName.service("View1Service", function($http,$q,RESOURCES){
+	
+	var urlBase = RESOURCES.PROD_DOMAIN;
 
 	this.saveUserSubscription = function(user){		
 
 		var defer = $q.defer();
-		return $http.post('http://localhost:8080/saveusersubscription',JSON.stringify(user))		
+		return $http.post(urlBase+'/saveusersubscription',JSON.stringify(user))		
 			.then(function(response) {
 				defer.resolve(response.data);
 				return defer.promise;
